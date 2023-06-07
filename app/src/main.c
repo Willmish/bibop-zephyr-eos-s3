f754d50604a33599ef205d881ef9db26f0d21312
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2023-06-07 14:29:44 +0200

    Trying with UART1.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -4,37 +4,46 @@
  */
 
 #include <zephyr/kernel.h>
-#include <sys/printk.h>
-#include <sys/util.h>
-#include <string.h>
-#include <usb/usb_device.h>
-#include <drivers/uart.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/gpio.h>
 
 #include "app_version.h"
 
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-        "Console device is not ACM CDC UART device");
 
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-    const struct device *usb_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-    uint32_t dtr = 0;
+	int ret;
 
-    if (usb_enable(NULL)) {
-        return;
-    }
-
-    while (!dtr) {
-        uart_line_ctrl_get(usb_dev, UART_LINE_CTRL_DTR, &dtr);
-        k_sleep(K_MSEC(100));
-    }
-
-	while (1) {
-        printk("Hello USB Console...\n");
-		k_sleep(K_MSEC(1000));
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
 	}
 
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+		//k_msleep(SLEEP_TIME_MS * 5);
+        //printk("Hello USB Console...\n");
+		k_msleep(SLEEP_TIME_MS);
+	}
 	return 0;
 }
-
