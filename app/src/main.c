a6ba632e596f8de89af51932477479ac15c5d99a
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2023-06-10 14:13:40 +0200

    Dummy I2C support.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -7,6 +7,9 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/device.h>
+#include <stdio.h>
 
 #include "app_version.h"
 
@@ -17,11 +20,65 @@
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
+static void fetch_and_display(const struct device *sensor)
+{
+	static unsigned int count;
+	struct sensor_value accel[3];
+	struct sensor_value temperature;
+	const char *overrun = "";
+	int rc = sensor_sample_fetch(sensor);
+
+	++count;
+	if (rc == -EBADMSG) {
+		/* Sample overrun.  Ignore in polled mode. */
+		if (IS_ENABLED(CONFIG_LIS2DH_TRIGGER)) {
+			overrun = "[OVERRUN] ";
+		}
+		rc = 0;
+	}
+	if (rc == 0) {
+		rc = sensor_channel_get(sensor,
+					SENSOR_CHAN_ACCEL_XYZ,
+					accel);
+	}
+	if (rc < 0) {
+		printf("ERROR: Update failed: %d\n", rc);
+	} else {
+		printf("#%u @ %u ms: %sx %f , y %f , z %f",
+		       count, k_uptime_get_32(), overrun,
+		       sensor_value_to_double(&accel[0]),
+		       sensor_value_to_double(&accel[1]),
+		       sensor_value_to_double(&accel[2]));
+	}
+
+	if (IS_ENABLED(CONFIG_LIS2DH_MEASURE_TEMPERATURE)) {
+		if (rc == 0) {
+			rc = sensor_channel_get(sensor, SENSOR_CHAN_DIE_TEMP, &temperature);
+			if (rc < 0) {
+				printf("\nERROR: Unable to read temperature:%d\n", rc);
+			} else {
+				printf(", t %f\n", sensor_value_to_double(&temperature));
+			}
+		}
+
+	} else {
+		printf("\n");
+	}
+}
+
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+#ifdef CONFIG_LIS2DH_TRIGGER
+static void trigger_handler(const struct device *dev,
+			    const struct sensor_trigger *trig)
+{
+	fetch_and_display(dev);
+}
+#endif
 
 int main(void)
 {
@@ -36,14 +93,60 @@ int main(void)
 		return 0;
 	}
 
-	while (1) {
+	const struct device *const sensor = DEVICE_DT_GET_ANY(st_lis2dh);
+
+	if (sensor == NULL) {
+		printf("No device found\n");
+		return 0;
+	}
+	if (!device_is_ready(sensor)) {
+		printf("Device %s is not ready\n", sensor->name);
+		return 0;
+	}
+
+#if CONFIG_LIS2DH_TRIGGER
+	{
+		struct sensor_trigger trig;
+		int rc;
+
+		trig.type = SENSOR_TRIG_DATA_READY;
+		trig.chan = SENSOR_CHAN_ACCEL_XYZ;
+
+		if (IS_ENABLED(CONFIG_LIS2DH_ODR_RUNTIME)) {
+			struct sensor_value odr = {
+				.val1 = 1,
+			};
+
+			rc = sensor_attr_set(sensor, trig.chan,
+					     SENSOR_ATTR_SAMPLING_FREQUENCY,
+					     &odr);
+			if (rc != 0) {
+				printf("Failed to set odr: %d\n", rc);
+				return 0;
+			}
+			printf("Sampling at %u Hz\n", odr.val1);
+		}
+
+		rc = sensor_trigger_set(sensor, &trig, trigger_handler);
+		if (rc != 0) {
+			printf("Failed to set trigger: %d\n", rc);
+			return 0;
+		}
+
+		printf("Waiting for triggers\n");
+		while (true) {
+			k_sleep(K_MSEC(2000));
+		}
+	}
+#else /* CONFIG_LIS2DH_TRIGGER */
+	printf("Polling at 0.5 Hz\n"); // Don't wait for trigger right now
+	while (true) {
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return 0;
 		}
-		//k_msleep(SLEEP_TIME_MS * 5);
-        //printk("Hello USB Console...\n");
-		k_msleep(SLEEP_TIME_MS);
+		fetch_and_display(sensor);
+		k_sleep(K_MSEC(2000));
 	}
-	return 0;
+#endif /* CONFIG_LIS2DH_TRIGGER */
 }
