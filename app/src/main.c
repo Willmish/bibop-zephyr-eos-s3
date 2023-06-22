a7a1f8bf4391d2e1b4a621b43a52c8cd72140949
Szymon Duchniewicz <szduchniewicz@gmail.com>
2023-06-22 13:00:37 +0100

    Add minimal working lis2dh working program.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -38,22 +38,60 @@
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
+static void fetch_and_display(const struct device *sensor)
+{
+	static unsigned int count;
+	struct sensor_value accel[3];
+	struct sensor_value temperature;
+	const char *overrun = "";
+	int rc = sensor_sample_fetch(sensor);
+
+	++count;
+	if (rc == -EBADMSG) {
+		/* Sample overrun.  Ignore in polled mode. */
+		if (IS_ENABLED(CONFIG_LIS2DH_TRIGGER)) {
+			overrun = "[OVERRUN] ";
+		}
+		rc = 0;
+	}
+	if (rc == 0) {
+		rc = sensor_channel_get(sensor,
+					SENSOR_CHAN_ACCEL_XYZ,
+					accel);
+	}
+	if (rc < 0) {
+		printf("ERROR: Update failed: %d\n", rc);
+	} else {
+		printf("#%u @ %u ms: %sx %f , y %f , z %f",
+		       count, k_uptime_get_32(), overrun,
+		       sensor_value_to_double(&accel[0]),
+		       sensor_value_to_double(&accel[1]),
+		       sensor_value_to_double(&accel[2]));
+	}
+
+	if (IS_ENABLED(CONFIG_LIS2DH_MEASURE_TEMPERATURE)) {
+		if (rc == 0) {
+			rc = sensor_channel_get(sensor, SENSOR_CHAN_DIE_TEMP, &temperature);
+			if (rc < 0) {
+				printf("\nERROR: Unable to read temperature:%d\n", rc);
+			} else {
+				printf(", t %f\n", sensor_value_to_double(&temperature));
+			}
+		}
+
+	} else {
+		printf("\n");
+	}
+}
+
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON0_NODE, gpios);
-const struct device *i2c_dev = DEVICE_DT_GET(I2C_NODE);
-
-//I2C_Config i2c0config =
-//{
-//    .eI2CFreq = I2C_400KHZ,
-//    .eI2CInt = I2C_DISABLE,
-//    .ucI2Cn = 0
-//};
+//const struct device *i2c_dev = DEVICE_DT_GET(I2C_NODE);
+const struct device *const sensor = DEVICE_DT_GET_ANY(st_lis2dh);
 
 int main(void)
 {
 	int ret;
-    //HAL_StatusTypeDef hal_status;
-    
 
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
@@ -64,34 +102,30 @@ int main(void)
         return 0;
     }
 
-    if (i2c_dev == NULL || !device_is_ready(i2c_dev)) {
-        printk("I2C device not ready.\n");
-        return 0;
-    }
+    //if (i2c_dev == NULL || !device_is_ready(i2c_dev)) {
+    //    printk("I2C device not ready.\n");
+    //    return 0;
+    //}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
-    
-    //ret = gpio_pin_configure_dt(&button, GPIO_INPUT); // TODO: THIS MESSES UP THE ACTUAL PINMUX????
-	if (ret < 0) {
-		return 0;
-	}
-    //printk("Reading button..\n");
-    //while (1) {
-	//		/* If we have an LED, match its state to the button's. */
-	//		int val = gpio_pin_get_dt(&button);
-    //        printk("%d", val);
-    //        //uint8_t val2;
-    //        //HAL_GPIO_Read(0, &val2);
-    //        //printk("%d", val2);
 
-	//		if (val >= 0) {
-	//			gpio_pin_set_dt(&led, val);
-	//		}
-	//		k_msleep(SLEEP_TIME_MS);
-	//	}
+    if (sensor == NULL) {
+        printk("No Lis2dh device found\n");
+        return 0;
+    }
+    if(!device_is_ready(sensor)) {
+        printk("Device %s is not ready\n", sensor->name);
+        return 0;
+    }
+    printk("Polling at 2Hz\n");
+    while(true) {
+        fetch_and_display(sensor);
+        k_sleep(K_MSEC(500));
+    }
+    /*
     printk("Initialising I2C..\n");
     uint8_t who_am_i = 0;
         
@@ -99,20 +133,26 @@ int main(void)
     if (ret == 0) {
     printk("LIS2DH12 WHO AM I: 0x%x\n", who_am_i);
     }
-
-    /*hal_status = HAL_I2C_Init(i2c0config);
-    if (hal_status != HAL_OK) {
-        printk("Failed to initialise I2C HAL interface! %x\n", hal_status);
-        return 0;
-    }
-    uint8_t rval = 0;
-    // check who am i lis2dh 
-    hal_status = HAL_I2C_Read(LIS2DH12_I2C_ADDR, LIS2DH12_WHO_AM_I, &rval, 1);
-    if (hal_status != HAL_OK) {
-        printk("Failed to read over I2C HAL interface! %x\n", hal_status);
-        return 0;
-    }
-    printk("LIS2DH12 WHO AM I: 0x%x\n", rval);
     */
+
+    /*ret = gpio_pin_configure_dt(&button, GPIO_INPUT); // TODO: THIS MESSES UP THE ACTUAL PINMUX????
+	if (ret < 0) {
+		return 0;
+	}
+    printk("Reading button..\n");
+    while (1) {
+	//	If we have an LED, match its state to the button's.
+			int val = gpio_pin_get_dt(&button);
+            printk("%d", val);
+            //uint8_t val2;
+            //HAL_GPIO_Read(0, &val2);
+            //printk("%d", val2);
+
+			if (val >= 0) {
+				gpio_pin_set_dt(&led, val);
+			}
+			k_msleep(SLEEP_TIME_MS);
+		}
+    	*/
     return 0;
 }
