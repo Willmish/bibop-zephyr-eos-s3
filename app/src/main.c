5df0cb2c970e8f309b5ada295d79a7e1fe4cf21f
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2023-06-25 22:43:19 +0200

    Start working with threads.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -25,6 +25,10 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
+#define STACKSIZE 1024
+#define PRIORITY 5
+#define THREAD_DELAY_START 1000
+
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 /* The devicetree node indentrifier for the "button0" alias */
@@ -38,7 +42,18 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON0_NODE, gpios);
 const struct device *const sensor_max = DEVICE_DT_GET_ANY(maxim_max30101);
 const struct device *dev_display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+
 int init_main();
+void sensor_task(void *, void *, void *);
+void inference_task(void *, void *, void *);
+
+/* Define threads */
+K_THREAD_DEFINE(sensor_task_id, STACKSIZE, sensor_task,
+                NULL, NULL, NULL,
+                PRIORITY, 0, THREAD_DELAY_START);
+K_THREAD_DEFINE(inference_task_id, STACKSIZE, inference_task,
+                NULL, NULL, NULL,
+                PRIORITY, 0, THREAD_DELAY_START);
 
 int main(void)
 {
@@ -59,17 +74,16 @@ int main(void)
      * NOTE: SENSOR_CHAN_IR on our board IS CONNECTED TO THE RED LED
      * and SENSOR_CHAN_RED is connected TO THE IR LED. So data is actually coming from the other place
      */
-    loop_model();
     while(1) {
-        bibop_get_mapped_values(sensor_max, &ir, &red);
-        if(ir.val2 == -128)
-        {
-            snprintk(print_buf, sizeof(print_buf), "Place your finger on the sensor");
-        }
-        else {
-            snprintk(print_buf, sizeof(print_buf), "IR value:   %d", ir.val2);
-        }
-        bdisplay_writetext(dev_display, &display_conf, print_buf);
+        //bibop_get_mapped_values(sensor_max, &ir, &red);
+        //if (ir.val2 == -128) {
+        //    snprintk(print_buf, sizeof(print_buf), "Place your finger on the sensor");
+        //}
+        //else {
+        //    snprintk(print_buf, sizeof(print_buf), "IR value:   %d", ir.val2);
+        //}
+        //bdisplay_writetext(dev_display, &display_conf, print_buf);
+        //k_sleep(K_MSEC(5)); // 5 ms intervals (200Hz)
         k_sleep(K_MSEC(5)); // 5 ms intervals (200Hz)
     }
     return 0;
@@ -81,7 +95,7 @@ int init_main() {
 		return 0;
 	}
 
-    if(!gpio_is_ready_dt(&button)) {
+    if (!gpio_is_ready_dt(&button)) {
         printk("Button device not ready.\n");
         return 0;
     }
@@ -94,16 +108,34 @@ int init_main() {
         printk("No Max30101/2 device found\n");
         return 0;
     }
-    if(!device_is_ready(sensor_max)) {
+    if (!device_is_ready(sensor_max)) {
         printk("Device %s is not ready\n", sensor_max->name);
         return 0;
     }
 
     setup_model();
     preprocess_data();
-    if(!device_is_ready(dev_display)) {
+    if (!device_is_ready(dev_display)) {
         printk("Device %s is not ready\n", dev_display->name);
         return 0;
     }
     return 1;
+}
+
+void sensor_task(void* p1, void *p2, void *p3)
+{
+    while (1) {
+        printk("hello from sensor_task\n");
+        k_sleep(K_MSEC(500));
+    }
+}
+
+void inference_task(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        //loop_model();
+        preprocess_data();
+        printk("hello from inference_task\n");
+        k_sleep(K_MSEC(1000));
+    }
 }
