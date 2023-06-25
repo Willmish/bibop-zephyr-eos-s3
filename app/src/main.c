cd6b9d41cc2ae4202dba0275ba68927a7d06d9ee
Szymon Duchniewicz <szduchniewicz@gmail.com>
2023-06-25 16:42:45 +0100

    Fix bonk and refactor init.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -18,7 +18,6 @@
 
 #include "bibop_display.h"
 #include "bibop_max30102.h"
-//#include "app_version.h"
 #include "model_functions.hpp"
 #include "bp_processing.hpp"
 
@@ -31,75 +30,27 @@
 /* The devicetree node indentrifier for the "button0" alias */
 #define BUTTON0_NODE DT_ALIAS(sw0)
 #define GPIO_NODE DT_NODELABEL(gpio)
-#define I2C_NODE DT_NODELABEL(i2c0)
 #if !DT_NODE_HAS_STATUS(GPIO_NODE, okay)
 #error "Unsupported board: gpio devicetree label is not defined"
 #endif
-#if !DT_NODE_HAS_STATUS(I2C_NODE, okay)
-#error "Unsupported board: I2C0 devicetree label is not defined"
-#endif
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON0_NODE, gpios);
-//const struct device *i2c_dev = DEVICE_DT_GET(I2C_NODE);
-//const struct device *const sensor = DEVICE_DT_GET_ANY(st_lis2dh);
 const struct device *const sensor_max = DEVICE_DT_GET_ANY(maxim_max30101);
 const struct device *dev_display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+int init_main();
 
 int main(void)
 {
-	int ret;
-
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
-
-    if(!gpio_is_ready_dt(&button)) {
-        printk("Button device not ready.\n");
-        return 0;
-    }
-
-    /*
-    if (i2c_dev == NULL || !device_is_ready(i2c_dev)) {
-        printk("I2C device not ready.\n");
-        return 0;
-    }
-    */
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
-    //if (sensor_max == NULL) {
-    //    printk("No Max30101/2 device found\n");
-    //    return 0;
-    //}
-    //if(!device_is_ready(sensor_max)) {
-    //    printk("Device %s is not ready\n", sensor_max->name);
-    //    return 0;
-    //}
-
-    setup_model();
-    preprocess_data();
-    if(!device_is_ready(dev_display)) {
-        printk("Device %s is not ready\n", dev_display->name);
-        return 0;
-    }
-
-     // DEMO display
     struct bibop_display_conf display_conf;
+    if(!init_main())
+        return 0;
     if (!bdisplay_init(dev_display, &display_conf)) {
         return 0;
     }
     /*
     bdisplay_loop(dev_display, &display_conf);
     */
-
 
     struct sensor_value ir;
     struct sensor_value red;
@@ -122,4 +73,37 @@ int main(void)
         k_sleep(K_MSEC(5)); // 5 ms intervals (200Hz)
     }
     return 0;
+}
+
+int init_main() {
+	int ret;
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+    if(!gpio_is_ready_dt(&button)) {
+        printk("Button device not ready.\n");
+        return 0;
+    }
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+    if (sensor_max == NULL) {
+        printk("No Max30101/2 device found\n");
+        return 0;
+    }
+    if(!device_is_ready(sensor_max)) {
+        printk("Device %s is not ready\n", sensor_max->name);
+        return 0;
+    }
+
+    setup_model();
+    preprocess_data();
+    if(!device_is_ready(dev_display)) {
+        printk("Device %s is not ready\n", dev_display->name);
+        return 0;
+    }
+    return 1;
 }
